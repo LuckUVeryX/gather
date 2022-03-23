8c55274d7e8b1d04de3aa0fda917c511e945f444
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-03-24 00:12:41 +0800

    adds theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'palette.dart';
+
+final ThemeData appTheme = ThemeData(colorSchemeSeed: Palette.primary, fontFamily: 'SFProRounded').copyWith(
+  primaryColor: Palette.primary,
+);
