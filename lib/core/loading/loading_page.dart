e520a412b4704a2df8f4896cce45a59daea63200
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2022-03-24 13:59:35 +0800

    Feat/loading page animations (#6)
    * adds loading assets and flutter hooks
    
    * adds loading assets to path
    
    * adds loading page

--- lib/core/loading/loading_page.dart
+++ lib/core/loading/loading_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../utils/utils.dart';
@@ -9,10 +10,7 @@ class LoadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final backgroundColor = Theme.of(context).primaryColor;
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: const Center(child: LoadingIndicator()),
-    );
+    return Scaffold(backgroundColor: backgroundColor, body: const Center(child: LoadingIndicator()));
   }
 }
 
@@ -23,14 +21,142 @@ class LoadingIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final theme = Theme.of(context);
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
+    return Stack(
+      fit: StackFit.loose,
+      alignment: Alignment.center,
       children: [
-        SvgPicture.asset(ImagePath.kAppIcon),
-        Text('gather', style: textTheme.displaySmall?.copyWith(fontWeight: FontWeight.w800, color: theme.cardColor)),
+        Padding(
+          padding: const EdgeInsets.only(top: _space + _logoSize),
+          child: Text(
+            'gather',
+            style: textTheme.displaySmall?.copyWith(fontWeight: FontWeight.w800, color: theme.cardColor),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(bottom: _space + _logoSize),
+          child: AnimatedLoading(),
+        ),
       ],
     );
   }
 }
+
+class AnimatedLoading extends HookWidget {
+  const AnimatedLoading({
+    Key? key,
+  }) : super(key: key);
+
+  static const duration = Duration(milliseconds: 5500);
+
+  static const logoOffset = 0.39;
+  static const calendarOffset = 0.135;
+  static const martiniOffset = -0.105;
+  static const mapMarkerOffset = -0.37;
+
+  static const staticWeight = 1.0;
+  // static const staticAnimWeight = 0.8;
+  static const staticAnimWeight = 0.3;
+
+  static const curve = Curves.easeOutBack;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final controller = useAnimationController(duration: duration)..repeat();
+
+    return Container(
+      width: _logoSize,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(color: theme.cardColor, shape: BoxShape.circle),
+      child: SlideTransition(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: ImagePath.loadingPaths.map((e) => _LoadingAsset(svgPath: e)).toList(),
+        ),
+        position: TweenSequence<Offset>(
+          [
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, logoOffset),
+                end: const Offset(0, logoOffset),
+              ),
+              weight: staticWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, logoOffset),
+                end: const Offset(0, calendarOffset),
+              ).chain(CurveTween(curve: curve)),
+              weight: staticAnimWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, calendarOffset),
+                end: const Offset(0, calendarOffset),
+              ),
+              weight: staticWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, calendarOffset),
+                end: const Offset(0, martiniOffset),
+              ).chain(CurveTween(curve: curve)),
+              weight: staticAnimWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, martiniOffset),
+                end: const Offset(0, martiniOffset),
+              ),
+              weight: staticWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, martiniOffset),
+                end: const Offset(0, mapMarkerOffset),
+              ).chain(CurveTween(curve: curve)),
+              weight: staticAnimWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, mapMarkerOffset),
+                end: const Offset(0, mapMarkerOffset),
+              ),
+              weight: staticWeight,
+            ),
+            TweenSequenceItem(
+              tween: Tween<Offset>(
+                begin: const Offset(0, mapMarkerOffset),
+                end: const Offset(0, logoOffset),
+              ).chain(CurveTween(curve: curve)),
+              weight: staticAnimWeight,
+            ),
+          ],
+        ).animate(controller),
+      ),
+    );
+  }
+}
+
+class _LoadingAsset extends StatelessWidget {
+  const _LoadingAsset({
+    Key? key,
+    required this.svgPath,
+  }) : super(key: key);
+
+  final String svgPath;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SvgPicture.asset(svgPath, color: theme.primaryColor),
+    );
+  }
+}
+
+const _logoSize = 70.0;
+const _space = 6.0;
--- lib/core/utils/image_path.dart
+++ lib/core/utils/image_path.dart
@@ -1,3 +1,12 @@
 class ImagePath {
   static const kAppIcon = 'assets/app/icon.svg';
+
+  static const kLoadingG = 'assets/loading/g.svg';
+  static const kLoadingCalendar = 'assets/loading/u_calendar-alt.svg';
+  static const kLoadingMartini = 'assets/loading/u_glass-martini-alt.svg';
+  static const kLoadingMapMarker = 'assets/loading/u_map-marker.svg';
+
+  static List<String> get loadingPaths => List.unmodifiable(
+        [kLoadingG, kLoadingCalendar, kLoadingMartini, kLoadingMapMarker],
+      );
 }
