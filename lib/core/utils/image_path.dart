ad6a552d332cf50946d22acad6fc9d4e586928c3
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-03-24 14:24:33 +0800

    adds icon assets

--- lib/core/utils/image_path.dart
+++ lib/core/utils/image_path.dart
@@ -1,6 +1,4 @@
 class ImagePath {
-  static const kAppIcon = 'assets/app/icon.svg';
-
   static const kLoadingG = 'assets/loading/g.svg';
   static const kLoadingCalendar = 'assets/loading/u_calendar-alt.svg';
   static const kLoadingMartini = 'assets/loading/u_glass-martini-alt.svg';
