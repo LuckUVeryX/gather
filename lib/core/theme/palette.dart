801ecab02d33124bb4d9fe5272f89d23933f6b71
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-03-27 15:39:38 +0800

    updates app theme

--- lib/core/theme/palette.dart
+++ lib/core/theme/palette.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/painting.dart';
 
 class Palette {
-  static const primary = Color(0XFFFA4A0C);
+  static const primary = Color(0xFFFA4A0C);
+  static const scaffoldBackground = Color(0xFFF2F2F2);
 }
